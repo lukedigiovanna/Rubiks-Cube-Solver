61ed7704ba926576bf387e7683009b414c0afd36
lukedigiovanna <43895803+lukedigiovanna@users.noreply.github.com>
2021-03-05 13:53:21 -0500

    convex detection

--- maybe_this_will_work.py
+++ maybe_this_will_work.py
@@ -9,9 +9,35 @@ Objective of this attempt:
 
 import cv2
 import numpy as np
+import random
 import colorclassification
+import json
+import requests
+import urllib
 
-image = cv2.imread("all rubiks images/rubiks_corners/0.JPG")
+image = cv2.imread("all rubiks images/rubiks_corners_no_background/0.png")
+image = cv2.imread("all rubiks images/rubiks_corners/1.JPG")
+
+def remove_background(image):
+    cv2.imwrite("1.jpg", image)
+    params = (
+        ('api_key', 'ak-shiny-lab-a046e44'),
+    )
+
+    files = {
+        'file': ('1.jpg', open('1.jpg', 'rb')),
+    }
+    response = requests.post('https://api.boring-images.ml/v1.0/transparent-net', params=params, files=files)
+    url = json.loads(response.text)["result"]
+
+    resp = urllib.request.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
+
+image = remove_background(image)
+cv2.imwrite("noback.png",image)
+exit()
 
 target_width = 180
 
@@ -22,35 +48,101 @@ image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
 hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 gray_image = cv2.cvtColor(hsv_image, cv2.COLOR_BGR2GRAY)
 
-threshold = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 3, 2)
+threshold = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 37, 2)
 dilation_kernel = np.ones((3,3))
 threshold = 255-cv2.dilate(threshold, dilation_kernel)
-
-# 
-# copy = np.zeros(dilation.shape,np.uint8)
-# for r in range(1, len(dilation)-1):
-#     for c in range(1, len(dilation[0])-1):
-#         # check all directly adjacent sides
-#         if dilation[r][c] == 0 and (dilation[r-1][c] == 255 or dilation[r+1][c] == 255 or dilation[r][c-1] == 255 or dilation[r][c+1] == 255):
-#             copy[r][c] = 255
 
 cache = []
 
 row_i = 0
 height, width = threshold.shape
 
+index = 0
+class Region:
+    def __init__(self, pixel_coordinates):
+        global index
+        self.pixel_list = np.array(pixel_coordinates)
+        if len(pixel_coordinates) < 75 or len(pixel_coordinates) > 1200:
+            return None
+        self.leftx = self.pixel_list[:,0][self.pixel_list[:,0].argmin()]
+        self.rightx = self.pixel_list[:,0][self.pixel_list[:,0].argmax()]
+        self.topy = self.pixel_list[:,1][self.pixel_list[:,1].argmin()]
+        self.bottomy = self.pixel_list[:,1][self.pixel_list[:,1].argmax()]
+        map_width = self.rightx - self.leftx + 1
+        map_height = self.bottomy - self.topy + 1
+        self.map = np.zeros((map_height,map_width), np.uint8)
+        for i in range(self.topy,self.bottomy+map_height):
+            for j in range(self.leftx,self.rightx+map_width):
+                if (j,i) in pixel_coordinates:
+                    self.map[i-self.topy][j-self.leftx] = 255
+        self.edges = np.zeros(self.map.shape, np.uint8)
+        self.contours = []
+        # make the edge map and check for convexity
+        for i in range(0, map_height):
+            for j in range(0, map_width):
+                if i == 0 or i == map_height - 1 or j == 0 or j == map_width - 1:
+                    self.edges[i][j] = self.map[i][j]
+                    if self.edges[i][j] == 255:
+                        self.contours.append((j+self.leftx, i+self.topy))
+                    continue
+                if self.map[i][j] == 255 and (self.map[i-1][j] == 0 or self.map[i+1][j] == 0 or self.map[i][j-1] == 0 or self.map[i][j+1] == 0):
+                    self.edges[i][j] = 255
+                    self.contours.append((j+self.leftx,i+self.topy))
+
+        # convex detection
+        self.is_convex = True
+        for i in range(0, map_height):
+            edge_count = 0
+            if self.map[i][0] == 255:
+                edge_count+=1
+            for j in range(0, map_width-1):
+                if self.map[i][j] == 0 and self.map[i][j+1] == 255:
+                    edge_count += 1
+                    if edge_count > 1:
+                        self.is_convex = False
+                        break
+            if not self.is_convex:
+                break # no need to keep checking if we already found out we are not convex
+        if self.is_convex:
+            for j in range(0, map_width):
+                edge_count = 0
+                if self.map[0][j] == 255:
+                    edge_count += 1
+                for i in range(0, map_height-1):
+                    if self.map[i][j] == 0 and self.map[i+1][j] == 255:
+                        edge_count += 1
+                        if edge_count > 1:
+                            self.is_convex = False
+                            break
+                if not self.is_convex:
+                    break
+
+        cv2.imwrite("test/region"+str(index)+".png",self.map)
+        cv2.imwrite("test/edges"+str(index)+".png",self.edges)
+        index+=1
+
+    def get_predicted_color(self):
+        return predict_color(self.pixel_list)
+
+    def get_area(self):
+        return len(self.pixel_list)
+
+    def is_touching_edge(self, image):
+        return self.leftx == 0 or self.rightx == image.shape[1] - 1 or self.topy == 0 or self.bottomy == image.shape[0] - 1
+
+
 # returns a list of the pixel coordinates that make up the region
-def find_region(x, y, region):
+def find_region(x, y, region, count=0):
     point = (x, y)
-    if x < 0 or x >= width or y < 0 or y >= height or point in cache or threshold[y][x] == 0:
+    if count >= 500 or x < 0 or x >= width or y < 0 or y >= height or point in cache or threshold[y][x] == 0:
         return []
     else:
         region.append(point)
         cache.append(point)
-        find_region(x + 1, y, region)
-        find_region(x - 1, y, region)
-        find_region(x, y + 1, region)
-        find_region(x, y - 1, region)
+        find_region(x + 1, y, region, count + 1)
+        find_region(x - 1, y, region, count + 1)
+        find_region(x, y + 1, region, count + 1)
+        find_region(x, y - 1, region, count + 1)
 
 def average_color(region):
     mean = [0,0,0]
@@ -65,7 +157,7 @@ def average_color(region):
 def predict_color(region):
     mean = average_color(region)
     label, color, confidence = colorclassification.predict_using_ratios(mean)
-    if confidence < 0.6:
+    if confidence < 0.5:
         return (255,0,255)
     else:
         return color
@@ -77,14 +169,20 @@ while row_i < height:
     while col_i < width:
         region = []
         find_region(col_i, row_i, region)
-        if len(region) <= 50:
+        region = Region(region)
+        if len(region.pixel_list) < 75 or len(region.pixel_list) > 1200 or region.is_touching_edge(image) or not region.is_convex:
             col_i += 1
             continue
-        print(len(region))
-        color = predict_color(region)
-        for p in region:
+        color = region.get_predicted_color()
+        # color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+        for p in region.pixel_list:
             blank[p[1]][p[0]] = color
+        cv2.rectangle(blank,(region.leftx,region.topy),(region.rightx,region.bottomy),(0,255,0),thickness=1)
+        for cnt_point in region.contours:
+            cv2.circle(image, cnt_point, 0, (0,255,0), thickness=1)
         col_i += 1
     row_i += 1
 
-cv2.imwrite("threshold.png", blank)
+cv2.imwrite("threshold.png",threshold)
+cv2.imwrite("regions.png", blank)
+cv2.imwrite("contours.png", image)
