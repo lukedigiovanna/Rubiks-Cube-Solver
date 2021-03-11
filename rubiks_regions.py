98d6685a9f5c3d407d196a699d6f4a510e9187e7
lukedigiovanna <43895803+lukedigiovanna@users.noreply.github.com>
2021-03-11 00:45:23 -0500

    putting the cube together

--- rubiks_regions.py
+++ rubiks_regions.py
@@ -8,13 +8,31 @@ import urllib
 
 target_width = 180
 
+color_dict = {
+    "red": (0,0,255),
+    "orange": (0,125,255),
+    "white": (255, 255, 255),
+    "yellow": (0,255,255),
+    "blue":(255,0,0),
+    "green":(0,255,0)
+}
+def save_face_image(image_name, face_labels):
+    output = np.zeros([300,300,3],np.uint8)
+    for i in range(3):
+        for j in range(3):
+            cv2.rectangle(output, (j * 100, i * 100), (j * 100 + 100, i * 100 + 100), color_dict[face_labels[i][j]], -1)
+            cv2.rectangle(output, (j * 100, i * 100), (j * 100 + 100, i * 100 + 100), (0,0,0), 5)
+    cv2.imwrite(image_name+".png", output)
+
 def remove_background(filepath):
+    image = cv2.imread(filepath)
+    cv2.imwrite("temp.jpg", image)
     params = (
         ('api_key', 'ak-shiny-lab-a046e44'),
     )
 
     files = {
-        'file': (filepath, open(filepath, 'rb')),
+        'file': ('temp.jpg', open('temp.jpg', 'rb')),
     }
     response = requests.post('https://api.boring-images.ml/v1.0/transparent-net', params=params, files=files)
     url = json.loads(response.text)["result"]
@@ -64,27 +82,81 @@ class Image:
 
         self.cache = []
 
+        self.mask = np.zeros(self.image.shape, np.uint8)
+
         row_i = 0
         while row_i < self.height:
             col_i = 0
             while col_i < self.width:
                 region = []
                 self.find_region(col_i, row_i, region)
-                region = Region(region)
+                region = Region(self, region)
                 if len(region.pixel_list) < 50 or len(region.pixel_list) > 500:
                     col_i += 1
                     continue
                 self.regions.append(region)
+                outline_color = (255,0,0)
                 if region.total_grade <= -5:
+                    outline_color = (0,255,0)
                     self.right_face_regions.append(region)
                 elif region.total_grade >= 5:
+                    outline_color = (0,0,255)
                     self.left_face_regions.append(region)
                 else:
                     self.top_face_regions.append(region)
+
+                for p in region.pixel_list:
+                    self.mask[p[1]][p[0]] = region.color
+                cv2.rectangle(self.mask,(region.leftx,region.topy),(region.rightx,region.bottomy),outline_color,thickness=1)
+                
+                cv2.circle(self.mask, region.center, 1, (255,255,255), thickness=2)
+
                 col_i += 1
             row_i += 1
 
+        self.top_face = [['white','white','white'],['white','white','white'],['white','white','white']]
+        self.left_face = [['white','white','white'],['white','white','white'],['white','white','white']]
+        self.right_face = [['white','white','white'],['white','white','white'],['white','white','white']]
+        self.top_face_regions.sort(key=lambda a: a.center[1])
+        self.top_face[0][0] = self.top_face_regions[0].color_label
+        second_row = [self.top_face_regions[1], self.top_face_regions[2]]
+        second_row.sort(key=lambda a: a.center[0])
+        self.top_face[0][1] = second_row[1].color_label
+        self.top_face[1][0] = second_row[0].color_label
+        third_row = [self.top_face_regions[3],self.top_face_regions[4],self.top_face_regions[5]]
+        third_row.sort(key=lambda a: a.center[0])
+        self.top_face[2][0] = third_row[0].color_label
+        self.top_face[1][1] = third_row[1].color_label
+        self.top_face[0][2] = third_row[2].color_label
+        fourth_row = [self.top_face_regions[6],self.top_face_regions[7]]
+        fourth_row.sort(key=lambda a: a.center[0])
+        self.top_face[2][1] = fourth_row[0].color_label
+        self.top_face[1][2] = fourth_row[1].color_label
+        self.top_face[2][2] = self.top_face_regions[8].color_label
 
+        self.left_face_regions.sort(key=lambda a: a.center[0])
+        first_col = [self.left_face_regions[0],self.left_face_regions[1],self.left_face_regions[2]]
+        first_col.sort(key=lambda a: a.center[1])
+        second_col = [self.left_face_regions[3],self.left_face_regions[4],self.left_face_regions[5]]
+        second_col.sort(key=lambda a: a.center[1])
+        third_col = [self.left_face_regions[6],self.left_face_regions[7],self.left_face_regions[8]]
+        third_col.sort(key=lambda a: a.center[1])
+        for i in range(3):
+            self.left_face[i][0] = first_col[i].color_label
+            self.left_face[i][1] = second_col[i].color_label
+            self.left_face[i][2] = third_col[i].color_label
+        
+        self.right_face_regions.sort(key=lambda a: a.center[0])
+        first_col = [self.right_face_regions[0],self.right_face_regions[1],self.right_face_regions[2]]
+        first_col.sort(key=lambda a: a.center[1])
+        second_col = [self.right_face_regions[3],self.right_face_regions[4],self.right_face_regions[5]]
+        second_col.sort(key=lambda a: a.center[1])
+        third_col = [self.right_face_regions[6],self.right_face_regions[7],self.right_face_regions[8]]
+        third_col.sort(key=lambda a: a.center[1])
+        for i in range(3):
+            self.right_face[i][0] = first_col[i].color_label
+            self.right_face[i][1] = second_col[i].color_label
+            self.right_face[i][2] = third_col[i].color_label   
 
     # returns a list of the pixel coordinates that make up the region
     def find_region(self, x, y, region, count=0):
